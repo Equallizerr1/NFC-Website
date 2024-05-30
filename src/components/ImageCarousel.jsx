a1b87971a02a6475c6188e3d9037e7cade6020de
Equallizerr1 <samharradine1994@gmail.com>
2024-05-30 23:22:20 +0100

    changed carousel image source from api call to static url

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,28 +1,17 @@
 import "bootstrap/dist/css/bootstrap.css";
 import Carousel from "react-bootstrap/Carousel";
-import { useEffect, useState } from "react";
-import { getMedia } from "../../utils/api";
 
 export const ImageCarousel = () => {
-	const [imgArray, setImgArray] = useState([])
-
-	useEffect(() => {
-		getMedia().then(({ data }) => {
-			setImgArray(data)
-		})
-	}, [])
-	console.log(imgArray)
 	return (
-		<div
+		<div id="carousel-container"
 			style={{
 				display: "block",
-				padding: 50,
 			}}>
 			<Carousel>
 				<Carousel.Item interval={1500}>
 					<img
 						className="d-block w-100"
-						src={imgArray[0].media_url}
+						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/368241695_327849173001364_7120904336374750951_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=ceJ0bb3Ntd0Q7kNvgFvW4NB&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYA2U2ONh1OTuQgvkjYyBejUgge8jBDu_kPMSr1e5b6IWw&oe=665EC947"}
 						alt="Image One"
 					/>
 					<Carousel.Caption>
@@ -33,12 +22,56 @@ export const ImageCarousel = () => {
 				<Carousel.Item interval={1500}>
 					<img
 						className="d-block w-100"
-						src={imgArray[1].media_url}
+						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/436428703_968717797914256_6257493648668864447_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=18de74&_nc_ohc=Ep6hq-Ea5hMQ7kNvgH11wIB&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYDPeGAWp-vfSUfARwGiBzjx32CrkwEYK6QboySNsqDKXw&oe=665EDD2C"}
 						alt="Image Two"
 					/>
 					<Carousel.Caption>
 						<h3>Label for second slide</h3>
 						<p>Sample Text for Image Two</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item interval={1500}>
+					<img
+						className="d-block w-100"
+						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/444434706_811500253845478_432667925481102393_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=18de74&_nc_ohc=_F2AT6RhHfAQ7kNvgG-utu6&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYCgVTRcYklB59DP9HEdJ11IJvJtVzSyUoz6eAZNsN3L3A&oe=665EAAC0"}
+						alt="Image Three"
+					/>
+					<Carousel.Caption>
+						<h3>Label for third slide</h3>
+						<p>Sample Text for Image Three</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item interval={1500}>
+					<img
+						className="d-block w-100"
+						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/354519071_988121442220967_158139795672551303_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=18de74&_nc_ohc=DNYkorsU1T0Q7kNvgEO0wC5&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYA3m_P_kPEZG07C1HgUDfVXBb7hO2T_-rHhVJ8EgQUcRA&oe=665EBC8E"}
+						alt="Image Four"
+					/>
+					<Carousel.Caption>
+						<h3>Label for fourth slide</h3>
+						<p>Sample Text for Image Four</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item interval={1500}>
+					<img
+						className="d-block w-100"
+						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/375475142_270440352606901_3643622405021606174_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=18de74&_nc_ohc=tBou6kL35rIQ7kNvgEls4cU&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYDnLR3tWpRHnFhaqfTCmA3V108Pk2dpMRYrTnolMUf_qQ&oe=665EB971"}
+						alt="Image Five"
+					/>
+					<Carousel.Caption>
+						<h3>Label for fifth slide</h3>
+						<p>Sample Text for Image Five</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item interval={1500}>
+					<img
+						className="d-block w-100"
+						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/374673955_1485585302257496_5764536069796501876_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=18de74&_nc_ohc=MIKF6rtNBRgQ7kNvgH6wulX&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYASp3uDHUXHQS7-MJtIIDkyNy_DKglvjB7zyDvqo8Iwog&oe=665EBB94"}
+						alt="Image Six"
+					/>
+					<Carousel.Caption>
+						<h3>Label for sixth slide</h3>
+						<p>Sample Text for Image Six</p>
 					</Carousel.Caption>
 				</Carousel.Item>
 			</Carousel>
--- utils/api.js
+++ utils/api.js
@@ -1,12 +1,27 @@
 import axios from "axios";
-import { IG_LONG_LIVED_ACCESS_TOKEN_NFC, IG_USER_ID_NFC } from "./api_details";
+import {
+	IG_LONG_LIVED_ACCESS_TOKEN_NFC,
+	IG_USER_ID_NFC,
+} from "./api_details.js";
 
 const nfcApi = axios.create({
-	baseURL: `https://graph.instagram.com/v15.0/${IG_USER_ID_NFC}/media?fields=id,caption,media_url,media_type,timestamp,children{media_url}&limit=10&access_token=${IG_LONG_LIVED_ACCESS_TOKEN_NFC}`,
+	baseURL: `https://graph.instagram.com/v15.0/${IG_USER_ID_NFC}/media?fields=id,caption,media_url,media_type,timestamp,children{media_url}&access_token=${IG_LONG_LIVED_ACCESS_TOKEN_NFC}`,
 });
 
+// const allPostsToSarah = axios.create({
+// 	baseURL: `https://graph.instagram.com/v15.0/${IG_USER_ID_NFC}/media?fields=id,caption,media_url,media_type,timestamp,children{media_url}&access_token=${IG_LONG_LIVED_ACCESS_TOKEN_NFC}`,
+// });
 export const getMedia = () => {
 	return nfcApi.get().then(({ data }) => {
+		return data;
+	});
+};
+const nextPosts = axios.create({
+	baseURL:
+		"https://graph.instagram.com/v20.0/17841445119364695/media?fields=id,caption,media_url,media_type,timestamp,children{media_url}&access_token=IGQWROSTBXMGZAyM2ZACamhLWHNDNVBwY3J6TnUtcGI1WEZAMamxNWWI3Um5QdXVwdlF4UjVHUTd3MDhPREdMZA0NnM1RxMklGUU9mN3JGNDBFdFd5bUNaR2ROYUpEQjd1VDJmcmwxU1RILVZAzeXlnQ1ZAoZAlp5YU1aVlEZD&pretty=1&limit=25&after=QVFIUjV1em9iNndodUZAnVkhQd0NHT1dEeFVVRF9tNDJfT1ZAnTFlUMXBQcy1lRE1tY3htUDVzMnJqTXRVZAWFrTjJTMlVqM3I5SXpNS19ENHM4bEctaVlnRHJB",
+});
+export const getNextPosts = () => {
+	return nextPosts.get().then(({ data }) => {
 		return data;
 	});
 };
