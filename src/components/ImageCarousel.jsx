12eef298d573016673d4b01c3bef47fb002dda81
Equallizerr1 <samharradine1994@gmail.com>
2024-06-06 15:16:57 +0100

    Commented carousel captions

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -15,10 +15,10 @@ export const ImageCarousel = () => {
 						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/368241695_327849173001364_7120904336374750951_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=ceJ0bb3Ntd0Q7kNvgFvW4NB&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYA2U2ONh1OTuQgvkjYyBejUgge8jBDu_kPMSr1e5b6IWw&oe=665EC947"}
 						alt="Image One"
 					/>
-					<Carousel.Caption>
+					{/* <Carousel.Caption>
 						<h3>Label for first slide</h3>
 						<p>Sample Text for Image One</p>
-					</Carousel.Caption>
+					</Carousel.Caption> */}
 				</Carousel.Item>
 				<Carousel.Item interval={1500}>
 					<img
@@ -26,10 +26,10 @@ export const ImageCarousel = () => {
 						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/436428703_968717797914256_6257493648668864447_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=18de74&_nc_ohc=Ep6hq-Ea5hMQ7kNvgH11wIB&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYDPeGAWp-vfSUfARwGiBzjx32CrkwEYK6QboySNsqDKXw&oe=665EDD2C"}
 						alt="Image Two"
 					/>
-					<Carousel.Caption>
+					{/* <Carousel.Caption>
 						<h3>Label for second slide</h3>
 						<p>Sample Text for Image Two</p>
-					</Carousel.Caption>
+					</Carousel.Caption> */}
 				</Carousel.Item>
 				<Carousel.Item interval={1500}>
 					<img
@@ -37,10 +37,10 @@ export const ImageCarousel = () => {
 						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/444434706_811500253845478_432667925481102393_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=18de74&_nc_ohc=_F2AT6RhHfAQ7kNvgG-utu6&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYCgVTRcYklB59DP9HEdJ11IJvJtVzSyUoz6eAZNsN3L3A&oe=665EAAC0"}
 						alt="Image Three"
 					/>
-					<Carousel.Caption>
+					{/* <Carousel.Caption>
 						<h3>Label for third slide</h3>
 						<p>Sample Text for Image Three</p>
-					</Carousel.Caption>
+					</Carousel.Caption> */}
 				</Carousel.Item>
 				<Carousel.Item interval={1500}>
 					<img
@@ -48,10 +48,10 @@ export const ImageCarousel = () => {
 						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/354519071_988121442220967_158139795672551303_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=18de74&_nc_ohc=DNYkorsU1T0Q7kNvgEO0wC5&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYA3m_P_kPEZG07C1HgUDfVXBb7hO2T_-rHhVJ8EgQUcRA&oe=665EBC8E"}
 						alt="Image Four"
 					/>
-					<Carousel.Caption>
+					{/* <Carousel.Caption>
 						<h3>Label for fourth slide</h3>
 						<p>Sample Text for Image Four</p>
-					</Carousel.Caption>
+					</Carousel.Caption> */}
 				</Carousel.Item>
 				<Carousel.Item interval={1500}>
 					<img
@@ -59,10 +59,10 @@ export const ImageCarousel = () => {
 						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/375475142_270440352606901_3643622405021606174_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=18de74&_nc_ohc=tBou6kL35rIQ7kNvgEls4cU&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYDnLR3tWpRHnFhaqfTCmA3V108Pk2dpMRYrTnolMUf_qQ&oe=665EB971"}
 						alt="Image Five"
 					/>
-					<Carousel.Caption>
+					{/* <Carousel.Caption>
 						<h3>Label for fifth slide</h3>
 						<p>Sample Text for Image Five</p>
-					</Carousel.Caption>
+					</Carousel.Caption> */}
 				</Carousel.Item>
 				<Carousel.Item interval={1500}>
 					<img
@@ -70,10 +70,10 @@ export const ImageCarousel = () => {
 						src={"https://scontent-man2-1.cdninstagram.com/v/t51.29350-15/405257575_1429563544262304_8594137721763410408_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=18de74&_nc_ohc=X3wK5-Kb06MQ7kNvgEvStBt&_nc_ht=scontent-man2-1.cdninstagram.com&edm=ANo9K5cEAAAA&oh=00_AYC9IU-vVu0QsPRUC2cNyTcqB52tcVl-B7sOuWsA2so6tA&oe=665EABBD"}
 						alt="Image Six"
 					/>
-					<Carousel.Caption>
+					{/* <Carousel.Caption>
 						<h3>Label for sixth slide</h3>
 						<p>Sample Text for Image Six</p>
-					</Carousel.Caption>
+					</Carousel.Caption> */}
 				</Carousel.Item>
 			</Carousel>
 		</div>
