898d75da3aca83eaefe813dcb89005685085cfaa
Equallizerr1 <samharradine1994@gmail.com>
2024-07-03 20:42:37 +0100

    changed image list variation to standard and added a backdrop to display image when clicked

--- src/components/Concerts.jsx
+++ src/components/Concerts.jsx
@@ -1,59 +1,55 @@
 // https://mui.com/material-ui/react-paper/ - try paper component behind images to give drop shadow
 import "../App.css";
-import Container from 'react-bootstrap/Container';
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import Container from 'react-bootstrap/Container';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { Grow, Fade } from '@mui/material';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import { useState } from "react";
 
 export const Concerts = ({ props }) => {
-    const [loaded, setLoaded] = useState([false]);
+    const [open, setOpen] = useState(false);
+    const [clickedImage, setClickedImage] = useState(null)
 
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            setLoaded(true);
-        }, 4000);
-        return () => clearTimeout(timeoutId);
-    }, []);
-
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleOpen = (e) => {
+        setClickedImage(e)
+        setOpen(true);
+    };
     return (
         <>
             <Container>
                 <h1>Concerts</h1>
             </Container>
-            <Box sx={{ overflowY: 'hidden', padding: 2 }}>
-                <ImageList variant="quilted" cols={4} gap={5}>
-                    {props.map((item) => (
-                        <>
-                            {/* <Fade in={loaded}> */}
-                            <Grow
-                                in={loaded}
-                                style={{ transformOrigin: "1 1 1" }}
-                                {...(loaded ? { timeout: 4000 } : {})}
-                            >
-                                <ImageListItem key={item.id}
-                                    sx={{
-                                        fill: (theme) => theme.palette.common.white,
-                                        stroke: (theme) => theme.palette.divider,
-                                        strokeWidth: 1,
-                                    }}>
-                                    <img
-                                        srcSet={`${item.link}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                        src={item.link}
-                                        //alt={item.caption}
-                                        loading="eager"
-                                        style={{ borderRadius: 5 }}
-                                    />
-                                </ImageListItem>
-                            </Grow>
-                            {/* </Fade> */}
-                        </>
-                    ))}
-                </ImageList>
-            </Box>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={open}
+                onClick={handleClose}
+            >
+                <img src={clickedImage} alt="" style={{ borderRadius: 5, maxHeight: "95%", maxWidth: "95%" }} />
+            </Backdrop>
+            <ImageList cols={3} gap={5} sx={{ padding: 5 }}>
+                {props.map((item) => (
+                    <ImageListItem key={item.id}
+                        sx={{
+                            fill: (theme) => theme.palette.common.white,
+                            stroke: (theme) => theme.palette.divider,
+                            strokeWidth: 1,
+                        }}>
+                        <img
+                            srcSet={item.link}
+                            src={item.link}
+                            //alt={item.caption}
+                            loading="lazy"
+                            style={{ borderRadius: 5 }}
+                            onClick={(e) => { handleOpen(e.target.currentSrc) }}
+                        />
+                    </ImageListItem>
+                ))}
+            </ImageList>
         </>
     )
 };
